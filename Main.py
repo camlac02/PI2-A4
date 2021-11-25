032687af2cc8a5bac8f2432c530da87856796f0f
Leodespt <83962953+Leodespt@users.noreply.github.com>
2021-11-25 17:09:26 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+from Actif import Actif
+from Connexion import Connexion
+
+if __name__=="__main__":
+    
+    print('in main')
+    connection = Connexion('pi2','root','*******')
+    connection.initialisation()
+
+    date_test="2016-07-01"
+    list_asset = Actif.creationActif(connection)
+    list_asset_with_value = Actif.Valeur_Actif(list_asset,date_test,connection)
+    for Asset in list_asset_with_value:
+        print(Asset)
+    
+    connection.close_connection()
