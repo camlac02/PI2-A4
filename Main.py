2461ebf779bf067e16805e019a81ee5d1517933b
Leodespt <83962953+Leodespt@users.noreply.github.com>
2021-11-30 12:58:28 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,15 +3,14 @@ from Connexion import Connexion
 
 if __name__=="__main__":
     
-    print('in main')
-    connection = Connexion('pi2','root','*******')
+    connection = Connexion('pi2','root','Leo20-Esilv')
     connection.initialisation()
 
     date_test="2016-07-01"
     list_asset = Actif.creationActif(connection)
     list_asset_with_value = Actif.Valeur_Actif(list_asset,date_test,connection)
-    for Asset in list_asset_with_value:
-        print(Asset.__repr__()) #N'affiche pas le __repr__() correctement
-
     
+    for Asset in list_asset_with_value:
+        print(Asset.__repr__()) 
+
     connection.close_connection()
