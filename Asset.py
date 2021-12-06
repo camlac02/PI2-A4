ebb3ba56ef614e5450afa281a9ae268ca382c857
PetruAgert <93982007+PetruAgert@users.noreply.github.com>
2021-12-06 17:40:43 +0100

    Update Asset.py

--- Asset.py
+++ Asset.py
@@ -11,9 +11,9 @@ class Asset():
 			ret.append((self.pt[i+1] - self.pt[i])/self.pt[i])
 		return(ret)
 	def ret_geo(self, ret):
-		return(np.prod([x+1 for x in ret])**(1/len(retours))-1)
+		return(np.prod([x+1 for x in ret])**(1/len(ret))-1)
 	
 	def std_dev(self, ret):
 		mean = sum(ret)/len(ret)
-		return(np.sqrt(sum([(x - mean)**2 for x in ret])/len(ret))]
+		return(np.sqrt(sum([(x - mean)**2 for x in ret])/len(ret)))
 	
