a20c930ca44734748d716f1064ea8f304ac41bde
PetruAgert <93982007+PetruAgert@users.noreply.github.com>
2021-12-06 16:44:53 +0100

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -1,1 +1,26 @@
 class Portfolio():
+  def __init__(self, ret):
+    self.ret = ret
+   
+  def ret_portfolio(self, prop):
+    ret_geo = []
+    for i in range(0, len(self.ret)):
+      ret_geo.append(np.prod([x+1 for x in self.ret[i]])**(1/len(self.ret[i])) - 1)
+     return(sum([a*b for a,b in zip(prop, ret_geo)]))
+  
+  def vol_pf(self, prop):
+    std = []
+    for i in self.ret:
+      mean = sum(i)/len(i)
+      std.append(np.sqrt(sum([(x-mean)**2 for x in i])/len(i)))
+    mean_prop = [a*b for a,b in zip(prop, std)]
+    cor = np.corrcoef(self, ret)
+    value = []
+    for j in range(0, len(mean_prop)):
+      value.append(sum[mean_prop[j]*mean_prop[x] ** cor[j,x] for x in range(0,len(mean_prop))]))
+    return(np.sqrt(sum(val)))
+  
+  def optimize_pf(self, ret_d):
+    bounds = ((0.0, 1.0),) * len(self.ret)
+    init = list(np;random.dirichlet(np.ones(len(self.ret)), size = 1)[0]
+    prop_opti = optimize.minimize(self.vol_pf, init, method = 'SLSQP', constraints = ({'type': 'eq', 'fun': lambda inputs: 1.0 - np.sum(inputs)}
