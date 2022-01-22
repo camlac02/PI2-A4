6200608dd568f8119f34fa5b0786129eb4dee4af
Leodespt <Leonard.desportes@edu.devinci.fr>
2022-01-22 14:04:31 +0100

    commit

--- Actifs.py
+++ Actifs.py
@@ -18,12 +18,12 @@ class Actifs():
         #Fonction qui prend en argument la connexion avec la base de données
         #La fonction retourne une liste d'actifs avec le nom de chaque actif 
         #Tout les autres attributs sont initialisée à 0     
-        requete = 'Select distinct Noms from cac'
+        requete = 'Select distinct name from cac'
         curseur = connexion.execute(requete)
         liste_Actifs = []
 
         for row in curseur:
-            action = Actifs(row['Noms'],0,0,0,0,0)
+            action = Actifs(row['name'],0,0,0,0,0)
             liste_Actifs.append(action)
             
         return liste_Actifs
@@ -34,14 +34,15 @@ class Actifs():
         #et la connexion avec la base de données
         #La fonction retourne une liste d'actifs, chacun asocié à une liste contenant
         #toutes les informations le concernant ('Noms', 'valeurs' et 'volumnes') à la date du jour 
-        requete = "Select valeurs,volumes from cac where Dates = '"+date+"' and Noms = '"+str(self.nom)+"';"
+        requete = "Select value,volume,Rendements from cac where date = '"+date+"' and  name = '"+str(self.nom)+"';"
         curseur = connection.execute(requete)
         row = curseur.fetchone()
 
-        self.valeur = row['valeurs']
-        self.volume = row['volumes']
+        self.valeur = row['value']
+        self.volume = row['volume']
         self.date = date
-        self.Rendement_Actif(connection)
+        self.rendement = row['Rendements']
+        #self.Rendement_Actif(connection)
 
         return self
     
@@ -51,27 +52,7 @@ class Actifs():
         return "Nom : {0}, Valeur : {1}, Nbr d'Actions : {2}, \nDate : {3}\nRendement : {4}".format(self.nom,self.valeur, self.nb_shares, self.date,self.rendement)
 
 
-    def Rendement_Actif(self,connexion):
-
-        # Retourne une liste de tuple (date,rendement en %) d'un actif
-        requete1 = "Select valeurs, Dates from cac where Noms = '"+self.nom+"';"
-        curseur = connexion.execute(requete1)
-        valeurs_precedente = 0 
-  
-        for row in curseur:
-            if valeurs_precedente == 0:
-                self.rendement = 0
-            else :
-                self.rendement = (round((row['valeurs'] - valeurs_precedente)/ valeurs_precedente *100,2))
-            
-            valeurs_precedente = row['valeurs']
-
-        return self
-
-    #.copy()
-
-    ##################################  FONCTION PAS UTILISEE ################################################
-
+    # Crée une copy d'un actifs pour pouvoir le modifier sans modifier sa sauvgarde précédente
     def copy(self):
         nom = self.nom
         valeur = self.valeur
@@ -82,22 +63,26 @@ class Actifs():
         Actif = Actifs(nom,valeur,volume,date,nb_shares,rendement)
         return Actif
 
-    def Rendement(connexion):
+    ##################################  FONCTION PAS UTILISEE ################################################
 
-        requete_creation_columne = "ALTER TABLE cac ADD rendement float;"
-        connexion.execute(requete_creation_columne)
+    def Rendement_Actif(self,connexion):
 
-        requete = "Select * from cac;"
-        curseur = connexion.execute(requete)
-
+        # Retourne une liste de tuple (date,rendement en %) d'un actif
+        requete1 = "Select value, date from cac where name = '"+self.nom+"';"
+        curseur = connexion.execute(requete1)
         valeurs_precedente = 0 
-
+  
         for row in curseur:
+            if valeurs_precedente == 0:
+                self.rendement = 0
+            else :
+                self.rendement = (round((row['value'] - valeurs_precedente)/ valeurs_precedente *100,2))
+            
+            valeurs_precedente = row['value']
 
-            requete2 = "INSERT INTO cac ('rendement') VALUES ("+ (round((row['valeurs'] - valeurs_precedente)/ valeurs_precedente *100,2)) +") ;"
-            valeurs_precedente = row['valeurs']
+        return self
 
-        return 0 
+
 
 '''
     def Volatilite_Actif(Nom_Actifs,connexion):
