f7a4be868cadd9c0c5bf99b4779e1bbfd8d2a2cc
Camille <lacroix.camille@orange.fr>
2022-01-24 13:40:22 +0100

    MaJ Fitness

--- Fitness.py
+++ Fitness.py
@@ -5,8 +5,6 @@ from math import sqrt
 from Connexion import Connexion
 from datetime import datetime
 
-#test
-
 class Fitness():
     
     def __init__(self, portfeuille, debut):
@@ -14,39 +12,73 @@ class Fitness():
         self.debut=debut
 
     def score(portefeuille, volatilite, rendement, debut, date):
+        vol=0
+        v=Fitness.Volatilite(portefeuille, debut, date)
+        vol = (v-volatilite)*(v-volatilite)
+
+        print("La volatitilité du portfeuille est de :",v,"%")
+        r=Fitness.rendement(Fitness.tab_value_portfolio(portefeuille,debut,date))
+        rend = (r-rendement)*(r-rendement)
+        print("Le rendement du portfeuille est de :",r,"%")
+        score = vol + rend
+        
+        
         v=Fitness.Volatilite(portefeuille, debut, date)
         return score
 
+    def TabActifs(nom, debut, date):
+        tableau=[]
+        debut = date_to_int(debut)
+        date = date_to_int(date)
+        #Requête pour récupérer tous les actifs entre la date de début et la date de test
+        requete = "SELECT * FROM cac where Date>="+str(debut)+" and Date<"+str(date)+";"
+        conn = Connexion.execute(requete)
+        curseur = conn.connection()
+        for ligne in curseur:
+            if (ligne[nom]!= 'PX_LAST'):
+                NbShares = re.sub('\,+', '.', ligne[nom])
+                if (NbShares=='#N/A N/A'):
+                    tableau.append(1)
+                else :
+                    tableau.append(float(NbShares))
+        conn.close_connection(curseur)
+        return tableau
+
+# Calcul de Volatilite
     def Volatilite(portfeuille, debut, date):
         taille=len(portfeuille.liste.Actifs)
         tab=[]
+        #Calcul de variation pour chaque actif
         for i in range(0,taille):
             name1 = portfeuille.liste.Actifs[i].name
-            a=Fitness.variation_actif(Fitness.tab_actif(name1,debut, date))
-            summ=0
+            a=Fitness.variation(Fitness.TabActifs(name1,debut, date))
+            somme=0
             for j in range (0,taille):
                 name2 = portfeuille.list_assets[j].name
-                b=Fitness.variation_actif(Fitness.tab_actif(name2,debut, date))
+                b=Fitness.variation(Fitness.TabActifs(name2,debut, date))
                 value = portfeuille.list_assets[i].percentage * portfeuille.list_assets[j].percentage * Covariance(a,b)
-                summ = summ + value
-            tab.append(summ)
+                somme = somme + value
+            tab.append(somme)
         var_potrfolio=0
         for t in range (0,len(tab)):
             var_potrfolio=var_potrfolio+tab[t]
         vol = sqrt(var_potrfolio)*100*sqrt(252)
         return vol
 
-    def variation_actif(tab_actif):
+# Calcul de Variation
+    def variation(tableau):
         tab_var=[]
-        size = len(tab_actif)
+        size = len(tableau)
+        #(VF-VI)/VI
         for i in range(1,size):
-            a=(tab_actif[i-1]/tab_actif[i]-1)
+            a=(tableau[i-1]/tableau[i]-1)
             tab_var.append(a)
         return tab_var
 
 
 #Fonctions pour calcul du Fitness
 
+#La moyenne est la somme des valeurs divisée par la quantité de valeurs
 def Moyenne(tableau):
     total=0
     for i in range(0,len(tableau)):
@@ -54,6 +86,7 @@ def Moyenne(tableau):
     moyenne = total/len(tableau)
     return moyenne
 
+#Idem pour la moyenne du score
 def MoyenneScore(population):
     taille=len(population.list_portefeuille)
     total=0
@@ -62,21 +95,19 @@ def MoyenneScore(population):
     moyenne = total/taille
     return moyenne
 
+#Calcul de covariance entre 2 tableaux 
 def Covariance(a,b):
+    #Vérification que les 2 tableaux ont la même taille
     if len(a) != len(b): 
-        return 0
-    Ma = Moyenne(a) 
-    Mb = Moyenne(b) 
+        return 0 
     somme = 0 
+    #Somme des (a(i)-Moyenne(a))(b(i)-Moyenne(b)) divisée par la longueur du tableau
     for i in range(0, len(a)): 
-        somme = somme+ ((a[i] - Ma) * (b[i] - Mb)) 
+        somme = somme+ ((a[i] - Moyenne(a)) * (b[i] - Moyenne(b))) 
     resultat = somme/(len(a))
     return resultat
 
-def Carre(a):
-    return a*a
-
-
+#Tri du tableau
 def Tri(tableau):
     for i in range(1,len(tableau)):
         element = tableau[i]
@@ -89,10 +120,12 @@ def Tri(tableau):
         tableau[j]=element
     return tableau         
 
+#Conversion d'un entier en date
 def int_to_date(nb):
     date=datetime.fromtimestamp(nb).strftime("%Y-%m-%d")
     return date
 
+#Conversion inverse de date en entier
 def date_to_int(date): #format en str : 01/01/2010
     date=datetime.strptime(date,"%d/%m/%Y")
     nb = int(date.strftime('%s'))+7200
