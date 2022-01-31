66bfc718e17d1ff8a27913cd8ff5faa6b0c62ced
helomalcles <93984455+helomalcles@users.noreply.github.com>
2022-01-31 20:26:14 +0100

    h

--- Population.py
+++ Population.py
@@ -1,6 +1,6 @@
 from Portefeuille import Portefeuille 
 from Actifs import Actifs
-
+from Connexion import Connexion
 class Population() :
 
     def __init__(self, list_portefeuille):
@@ -37,6 +37,35 @@ class Population() :
         
         return self
     
+    #Calcul des rendements moyens entre 2 dates
+
+    def MoyenneRendements(self,Connexion,date1,date2):
+        Liste=[]
+        requete1="Select distinct name from helo;"
+        curseur=Connexion.execute(requete1)
+        ListeNoms=[]
+        #on récupère la liste des noms des actifs
+        for row in curseur:
+            ListeNoms.append(row['name'])
+        for name in ListeNoms:
+            somme=0
+            requete2="select Rendements from helo where name='"+name+"' and date between '"+date1+"' and '"+date2+"';"
+            #on récupère la liste des Rendements de chaque actif
+            curseur2=Connexion.execute(requete2)    
+            nbrow=0
+            for row in curseur2:
+                somme+=(row['Rendements'])
+                nbrow+=1
+            moyenne=somme/nbrow
+            Liste.append(moyenne)
+        print(Liste)
 
     def __repr__(self):
         return "{0}".format(self.list_portefeuille)
+
+if __name__=="__main__":
+    pop = Population ([])
+    connection = Connexion('cac','root','Jhanamal0004@')
+    connection.initialisation()
+    pop.MoyenneRendements(connection,"2017-11-09","2017-11-17")
+    connection.close_connection()
