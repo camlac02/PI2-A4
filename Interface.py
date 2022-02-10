0898246474c8b2a69c3cf73bf4fbc3c7f43bf140
Camille Lacroix <lacroix.camille@orange.fr>
2022-02-10 21:47:11 +0100

    Interface

--- Interface.py
+++ Interface.py
@@ -3,15 +3,14 @@ from tkinter import *
 fenetre=Tk()
 fenetre.title("Risque maximal")
 
-Label = Label(fenetre, texte = "Quel est le risque maximal souhaité")
+Label = Label(fenetre, text = "Quel est le risque maximal souhaité")
 Label.pack()
 
 risk = StringVar()
-risk.set("0.1")
-saisie = Entry(fenetre, textevariable = risk, width = 10)
+saisie = Entry(fenetre, textvariable = risk, width = 10)
 saisie.pack()
 
-bouton1 = Button(fenetre, texte = "CALCULER", width=8)
+bouton1 = Button(fenetre, text = "CALCULER", width=8)
 bouton1.pack()
 
 fenetre.mainloop()
