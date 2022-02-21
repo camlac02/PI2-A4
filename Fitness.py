5b4a1f175e15ae788d22de7d31ffd420c23bc27a
PetruAgert <93982007+PetruAgert@users.noreply.github.com>
2022-02-21 13:22:14 +0100

    Update Fitness.py

--- Fitness.py
+++ Fitness.py
@@ -13,20 +13,7 @@ class fitness():
     def __init__(self, liste_Actifs, sharpe):
         self.liste_Actifs = liste_Actifs
         self.sharpe=sharpe
-        
-    def contrainte(portefeuille):
-        connexion = Connexion('CAC40','root','Petruluigi0405@!')
-        contrainte_limite = 0
-        for i in portefeuille:
-            contrainte_limite += i.rendements * i.nb_shares
-        contrainte_limite = (contrainte_limite - 1)**2
-        for i in portefeuille:
-            requete = "Select rendements from CAC40 where noms ='"+ i.noms +"';"
-            rendements_actif = connexion.execute(requete)
-            for row in rendements_actif:
-                contrainte_limite += max(0,row['rendements']-1)**2 +  max(0,row['rendements'])**2
-        return contrainte_limite
     
     def heuristique(portefeuille):     ##Fonction à minimiser
-        obj = -portefeuille.RatioSharpe() + 100*portefeuille.contrainte()
+        obj = -portefeuille.RatioSharpe() + 100*portefeuille.Contrainte()
         return obj
