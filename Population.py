7759c76f692e336be09bac0feb538ed8603a986b
Camille Lacroix <lacroix.camille@orange.fr>
2022-02-23 08:41:17 +0100

    MaJ

--- Population.py
+++ Population.py
@@ -3,10 +3,11 @@ from Actifs import Actifs
 from Connexion import Connexion
 import random
 class Population() :
-
+    #Attributs
     def __init__(self, list_portefeuille):
         self.list_portefeuille = list_portefeuille
 
+    #Création d'une population grâce à la liste des actifs et l'investissement maximal
     def Creation_Population(self, list_asset, MaxInvest, nb_portefeuille):
         
         list_portefeuille = []
@@ -22,10 +23,10 @@ class Population() :
         return self
     
     #Calcul des rendements moyens entre 2 dates
-
+    
+    '''
      #Tri les portefeuils d'une population en fonction de leurs scores
     def sort_population(self):
-
         for i in range(len(self.list_portefeuille)):
             for j in range(len(self.list_portefeuille)-1):
                 if(self.list_portefeuille[j].score < self.list_portefeuille[j+1].score):
@@ -33,30 +34,20 @@ class Population() :
                     self.list_portefeuille[j].score = self.list_portefeuille[j+1].score
                     self.list_portefeuille[j+1].score = score
         return self
-
     # def mutation_portefeuille(self,index,MaxInvest):
-
     #     self.list_portefeuille[index].mutation(MaxInvest)
     #     return self
-
     def crossover(self):
-
         return self
-
-
     def nouvelle_population(pop_precedente,list_asset,MaxInvest):
         
         Pourcentage_garder = 0.5
         new_list_portefeuille = []
-
         new_list_portefeuille.append(pop_precedente.list_portefeuille[0])
-
         for i in range(1,len(pop_precedente.list_portefeuille)):
             
             if i < len(pop_precedente.list_portefeuille)*Pourcentage_garder:
-
                 rnd = round(random.uniform(1,2))
-
                 if rnd == 1:
                     new_list_portefeuille.append(pop_precedente.list_portefeuille[i].mutation(MaxInvest))
                 if rnd == 2: 
@@ -64,17 +55,16 @@ class Population() :
                     #new_list_portefeuille.append(pop_precedente.mutation_portefeuille(MaxInvest).list_portefeuille)
             else :
                 new_list_portefeuille.append(Portefeuille(list_asset,0,0,0,0).Creation_Portefeuille(MaxInvest))
-
             new_list_portefeuille[i].RatioSharpe()
-
         return new_list_portefeuille 
+        '''
 
     def __repr__(self):
         return "{0}".format(self.list_portefeuille)
 
 if __name__=="__main__":
     pop = Population ([])
-    connection = Connexion('pi2','root','root')
+    connection = Connexion('cac','root','Jhanamal0004@')
     connection.initialisation()
     pop.MoyenneRendements(connection,"2017-11-09","2017-11-17")
     connection.close_connection()
