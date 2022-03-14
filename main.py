c652bf31cf6a3f7c6a32f4a3e7434621874ff541
PetruAgert <93982007+PetruAgert@users.noreply.github.com>
2022-03-14 11:42:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,68 +4,53 @@ Created on Thu Feb 17 12:57:23 2022
 
 @author: PC
 """
-
-from math import nan
+from AlgoG import AlgoG
 from Connexion import Connexion
 from Actifs import Actifs
-#from Population import Population
-from Portefeuille import Portefeuille
 from Population import Population
-from pandas import DataFrame
-import pandas
-import csv
+from Portefeuille import Portefeuille
+
 if __name__=="__main__":
-    
+
     #Connexion a la base de donnée
     connection = Connexion('BDD','root','PetruLuigi0405@!')
     connection.initialisation()
 
     #Date de création du portefeuille
-    date1 = "2018-11-01"
-    date2="2018-11-30"
+    date_1 = "2017-01-05"
+    date_2 = "2017-12-29"
+
     #Valeur Max de l'investissement
-    max_invest = 250000
+    max_invest = 300000
+    
+    # Saisie utilisateur
+    expected_return = float(input("Quel rendement souhaitez vous atteindre ? :"))
+    expected_std = float(input("Quel volatilité souhaitez vous atteindre ? :"))
     
     #Nombre de portefeuilles par population
     nb_portefeuils = 4
 
-    
     list_asset_with_value = []
-    
 
     list_asset = Actifs.creationActifs(connection)
 
     #Associe une valeur a chaque actif
     for asset in list_asset:
-            list_asset_with_value.append(asset.Valeur_Actifs(date1,date2,connection))
+            list_asset_with_value.append(asset.Valeur_Actifs(date_1,date_2,connection))
+            #list_asset_with_value.append(asset.Valeur_Actifs(date_1,connection))
 
-    #print(list_asset_with_value)
-    #Creation de la population
-    
     pop = Population([]) 
 
-    print('creation des portefeuilles')
-
-    pop.Creation_Population(list_asset_with_value, max_invest,nb_portefeuils)#connection,date_test)
-    #pop.Creation_Population(nb_portefeuils, max_invest,connection,date_test)
+    pop.creation_population( list_asset_with_value, max_invest,nb_portefeuils)
 
     print(pop.__repr__())
 
-    #affichage liste des rendements moyens sur une periode par actif
-    #pop.MoyenneRendements(connection,"2017-11-09")
 
+    Generation_max = 10
     
-    pop.sort_population()
-    print(pop.__repr__())
-    
-    Generation_max = 5
+        #Appel de l'algo G
+    algoG = AlgoG(pop,Generation_max).algorihtme_genetique(list_asset_with_value, max_invest,expected_return, expected_std)
 
-    for i in range(Generation_max):
-        
-        print('Generation : '+str(i))
-        pop = Population(Population.nouvelle_population(pop,list_asset_with_value,max_invest))
-        pop.sort_population()
-
-        print(pop.__repr__())
+    print('\nPortefeuille final :\n'+algoG.__repr__())
 
     connection.close_connection()
