eebb8b34b3efff140eca1bf946059407039e1ee0
Leodespt <83962953+Leodespt@users.noreply.github.com>
2022-03-26 11:36:19 +0100

    Create BDD.sql

---
+++ BDD.sql
@@ -0,0 +1,20 @@
+drop table cac;
+
+create table cac
+(
+date date,
+value float,
+volume int,
+name varchar(6),
+Rendements float);
+       
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/datacac.csv' 
+INTO TABLE cac 
+FIELDS TERMINATED BY ';'
+LINES TERMINATED BY '\r\n'
+Ignore 1 lines
+(@date,value,volume,name,Rendements)
+SET date = STR_TO_DATE(@date, '%d/%m/%Y');
+
+
+select * from cac;
